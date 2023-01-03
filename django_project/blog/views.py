19ed295d771c76e26765e5ffe8322c0e36036402
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-01-03 21:58:46 +0300

    Add content to home.html

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 1, 2023',
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 2, 2023',
+    },
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
